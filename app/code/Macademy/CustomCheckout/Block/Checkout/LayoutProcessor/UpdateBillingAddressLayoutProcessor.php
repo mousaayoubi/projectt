fd0c0cd66f0fbeceb00bcb840ff30ad2966050fc
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-08 15:03:55 +0200

    Update files

---
+++ app/code/Macademy/CustomCheckout/Block/Checkout/LayoutProcessor/UpdateBillingAddressLayoutProcessor.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\CustomCheckout\Block\Checkout\LayoutProcessor;
+
+use Magento\Checkout\Block\Checkout\LayoutProcessorInterface;
+
+class UpdateBillingAddressLayoutProcessor implements LayoutProcessorInterface
+{
+
+public function process($jsLayout){
+
+foreach ($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'] as &$paymentMethod){
+
+$fields = &$paymentMethod['children']['form-fields']['children'];
+
+if ($fields === null){
+continue;
+}
+
+$fields['city']['sortOrder'] = '72';
+$fields['region_id']['sortOrder'] = '74';
+$fields['postcode']['sortOrder'] = '76';
+
+}
+
+return $jsLayout;
+
+}
+}
---
+++ app/code/Macademy/CustomCheckout/registration.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+ComponentRegistrar::MODULE, 'Macademy_CustomCheckout',
+__DIR__
+);
+
+?>
+
+
