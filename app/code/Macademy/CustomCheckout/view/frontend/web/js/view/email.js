2b27d995ffbf183df9c4cf24fe8ab2b2911980df
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-10 13:30:06 +0200

    Update files

---
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/email.js
@@ -0,0 +1,26 @@
+define([
+'uiComponent',
+'ko',
+'Magento_Checkout/js/model/step-navigator',
+'mage/translate',
+'underscore',
+], function(Component, ko, stepNavigator, $t, _){
+'use strict';
+
+return Component.extend({
+defaults: {
+template: 'Macademy_CustomCheckout/email',
+isVisible: ko.observable(false),
+},
+initialize: function(){
+this._super();
+console.log('The email component has been loaded successfully.');
+stepNavigator.registerStep('email', null, $t('Email'), this.isVisible, _.bind(this.navigate, this), this.sortOrder);
+
+return this;
+},
+navigate: function(){
+return this.isVisible(true);
+},
+})
+})
