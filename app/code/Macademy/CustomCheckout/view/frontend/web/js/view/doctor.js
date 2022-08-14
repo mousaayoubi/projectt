4b135e06ee374eb6778242ccea40321e219d3027
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-14 15:35:39 +0200

    Update files

---
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/doctor.js
@@ -0,0 +1,24 @@
+define([
+'uiComponent',
+'Magento_Checkout/js/model/step-navigator',
+'mage/translate',
+'ko',
+'underscore',
+], function(Component, stepNavigator, $t, ko, _){
+'use strict';
+
+return Component.extend({
+defaults: {
+template: 'Macademy_CustomCheckout/doctor',
+isVisible: ko.observable(false),
+},
+initialize: function(){
+this._super();
+console.log('The doctor component has been loaded successfully.');
+stepNavigator.registerStep('doctor', null, $t('Doctor Info'), this.isVisible, _.bind(this.navigate, this), this.sortOrder);
+},
+navigate: function(){
+return this.isVisible(true);
+},
+})
+})
