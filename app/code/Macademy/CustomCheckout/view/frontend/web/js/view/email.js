368b9f8ada862fb02de10de43b98cc559ce36750
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-14 11:49:08 +0200

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/web/js/view/email.js
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/email.js
@@ -5,7 +5,9 @@ define([
 'mage/translate',
 'underscore',
 'Magento_Checkout/js/model/quote',
-], function(Component, ko, stepNavigator, $t, _, quote){
+'Magento_Customer/js/model/customer',
+'jquery',
+], function(Component, ko, stepNavigator, $t, _, quote, customer, $){
 'use strict';
 
 return Component.extend({
@@ -25,7 +27,21 @@ navigate: function(){
 return this.isVisible(true);
 },
 onSubmit: function(){
+if (this.validateEmail()){
 return stepNavigator.next();
+}
+},
+validateEmail: function(){
+const loginFormSelector = 'form[data-role=email-with-possible-login]';
+let emailValidationResult = customer.isLoggedIn();
+
+if (!customer.isLoggedIn()){
+$(loginFormSelector).validation();
+emailValidationResult = Boolean($(loginFormSelector + ' input[name=username]').valid());
+}
+
+return emailValidationResult;
+
 },
 })
 })
