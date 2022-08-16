684f844c2eae0d29878e134dc82cfa9fb368b0af
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-16 16:22:34 +0200

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/requirejs-config.js
+++ app/code/Macademy/CustomCheckout/view/frontend/requirejs-config.js
@@ -2,4 +2,10 @@ var config = {
 deps: [
 'Macademy_CustomCheckout/js/mask-input-telephone'
 ],
+map: {
+'*': {
+'Magento_Checkout/template/sidebar.html':
+'Macademy_CustomCheckout/template/sidebar.html'
+},
+}
 }
