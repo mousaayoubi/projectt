01200c6776638ac52e74977bf2c0f4d46b04ecbe
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-17 18:35:37 +0200

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/web/js/view/banner.js
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/banner.js
@@ -1,12 +1,18 @@
 define([
-'uiComponent'
-], function(Component){
+'uiComponent',
+'ko',
+'Magento_Customer/js/model/customer',
+], function(Component, ko, customer){
 'use strict';
 
 return Component.extend({
+defaults: {
+isVisible: ko.observable(customer.isLoggedIn()),
+},
 initialize: function(){
 this._super();
 console.log('The banner component has been loaded successfully.');
+console.log('Customer logged in: ' + this.isVisible());
 },
 })
 })
