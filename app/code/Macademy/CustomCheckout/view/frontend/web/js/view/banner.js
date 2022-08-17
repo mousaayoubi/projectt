699cf092cf2494532d6b6a250e0bf2f543904ee9
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-17 16:09:34 +0200

    Update files

---
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/banner.js
@@ -0,0 +1,12 @@
+define([
+'uiComponent'
+], function(Component){
+'use strict';
+
+return Component.extend({
+initialize: function(){
+this._super();
+console.log('The banner component has been loaded successfully.');
+},
+})
+})
