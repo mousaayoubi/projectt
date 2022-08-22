ae6a2d4d418f99e72228c2477e4646bd57ce677b
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-22 15:40:20 +0200

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/web/js/view/banner.js
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/banner.js
@@ -9,19 +9,12 @@ define([
 return Component.extend({
 defaults: {
 isVisible: ko.observable(customer.isLoggedIn()),
-fname: 'hi world',
 },
 initialize: function(){
 this._super();
 this.customer = customerData.get('customer');
 console.log('The banner component has been loaded successfully.');
 console.log('Customer logged in: ' + this.isVisible());
-console.log('Customer First Name: ' + this.firstname());
-},
-firstname: function(){
-var customerInfo = customerData.get('customer')();
-var customerFirstName = customerInfo.firstname;
-return customerFirstName;
 },
 })
 })
