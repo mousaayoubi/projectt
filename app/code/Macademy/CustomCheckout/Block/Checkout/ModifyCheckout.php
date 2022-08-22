9daffca0922b789e87f2e6b6eade1d0b70234416
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-22 19:02:49 +0200

    Update files

---
+++ app/code/Macademy/CustomCheckout/Block/Checkout/ModifyCheckout.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\CustomCheckout\Block\Checkout;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Backend\Block\Template\Context;
+
+class ModifyCheckout extends Template {
+
+protected $storeManager;
+
+public function __construct(StoreManagerInterface $storeManager, Context $context, array $data = []){
+
+$this->storeManager = $storeManager;
+
+parent::__construct($context, $data);
+
+}
+
+public function getStoreName(){
+
+return $this->storeManager->getStore()->getName();
+
+}
+}
