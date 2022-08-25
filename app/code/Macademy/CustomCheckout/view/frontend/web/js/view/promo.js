a908c729e46ab9057ce6b0505574cea513e5111c
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-25 14:34:41 +0200

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/web/js/view/promo.js
+++ app/code/Macademy/CustomCheckout/view/frontend/web/js/view/promo.js
@@ -1,12 +1,20 @@
 define([
 'uiComponent',
-], function(Component){
+'ko',
+'Magento_Checkout/js/model/quote',
+], function(Component, quote){
 'use strict';
 
 return Component.extend({
+defaults: {
+total: '$100',
+},
 initialize: function(){
 this._super();
 console.log('The promo component has been loaded successfully.');
+},
+grandTotal: function(){
+return '$100';
 },
 })
 })
