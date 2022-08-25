4c4e147060f118783df33e6df9766e5ea5867173
Mousa Ayoubi <mousaayoubi@gmail.com>
2022-08-25 11:21:06 +0200

    Update files

--- app/code/Macademy/CustomCheckout/Block/Checkout/ModifyCheckout.php
+++ app/code/Macademy/CustomCheckout/Block/Checkout/ModifyCheckout.php
@@ -3,19 +3,16 @@
 namespace Macademy\CustomCheckout\Block\Checkout;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Store\Model\StoreManagerInterface;
 use Magento\Store\Model\Information;
 use Magento\Backend\Block\Template\Context;
 use Magento\Framework\App\ObjectManager;
 
 class ModifyCheckout extends Template {
 
-protected $storeManager;
 protected $storeInfo;
 
-public function __construct(StoreManagerInterface $storeManager, Information $storeInfo, Context $context, array $data = []){
+public function __construct(Information $storeInfo, Context $context, array $data = []){
 
-$this->storeManager = $storeManager;
 $this->storeInfo = $storeInfo;
 
 parent::__construct($context, $data);
@@ -24,7 +21,11 @@ parent::__construct($context, $data);
 
 public function getStorePhone(){
 
-return $this->storeInfo->getStoreInformationObject($this->storeManager->getStore())->getCity();
+$storeManager = ObjectManager::getInstance()->get(\Magento\Store\Model\StoreManagerInterface::class);
+
+$getStore = $storeManager->getStore();
+
+return $this->storeInfo->getStoreInformationObject($getStore)->getPhone();
 
 }
 }
