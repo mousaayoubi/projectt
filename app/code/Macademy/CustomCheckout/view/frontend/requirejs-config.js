74dce4eb22c8cce9a8cb0ad1d921c56ce7b56961
Mousa Ayoubi <mousaayoubi@gmail.com>
2023-01-25 16:59:45 +0100

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/requirejs-config.js
+++ app/code/Macademy/CustomCheckout/view/frontend/requirejs-config.js
@@ -2,4 +2,12 @@ var config = {
 deps: [
 'Macademy_CustomCheckout/js/mask-input-telephone'
 ],
+config: {
+mixins: {
+'Magento_Checkout/js/action/set-shipping-information':
+{
+'Macademy_CustomCheckout/js/action/set-shipping-information-mixin': true
+}
+}
+},
 }
