3c7ff4bd338108fde43f5edabd2032a037694c81
Mousa Ayoubi <mousaayoubi@gmail.com>
2023-01-26 17:38:27 +0100

    Update files

--- app/code/Macademy/CustomCheckout/Block/Checkout/LayoutProcessor/UpdateShippingAttributeLayoutProcessor.php
+++ app/code/Macademy/CustomCheckout/Block/Checkout/LayoutProcessor/UpdateShippingAttributeLayoutProcessor.php
@@ -11,10 +11,24 @@ public function process($jsLayout)
 {
 
 $attributeCode = 'address_classification';
+$attributeCodeTest = 'address_test';
 
 $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children']['address_classification']['config']['customScope'] = 'shippingAddress.custom_attributes';
 
 $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children']['address_classification']['dataScope'] = "shippingAddress.custom_attributes.$attributeCode";
+
+foreach ($jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'] as &$paymentMethod){
+
+$fields = &$paymentMethod['children']['form-fields']['children'];
+
+if (isset($fields[$attributeCode])){
+unset($fields[$attributeCode]);
+}
+
+if (isset($fields[$attributeCodeTest])){
+unset($fields[$attributeCodeTest]);
+}
+}
 
 return $jsLayout;
 
