1cbbbeccf589156f1f104fd76f7869e2b19c2778
Mousa Ayoubi <mousaayoubi@gmail.com>
2023-01-26 12:38:34 +0100

    Update files

---
+++ app/code/Macademy/CustomCheckout/Plugin/ShippingAddressClassification.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\CustomCheckout\Plugin;
+
+use Magento\Checkout\Api\ShippingInformationManagementInterface;
+use Magento\Checkout\Api\Data\ShippingInformationInterface;
+
+class ShippingAddressClassification
+{
+
+public function beforeSaveAddressInformation(
+ShippingInformationManagementInterface $subject,
+$cartId,
+ShippingInformationInterface $addressInformation
+)
+{
+
+$shippingAddress = $addressInformation->getShippingAddress();
+
+if ($extensionAttributes = $shippingAddress->getExtensionAttributes()){
+$shippingAddress->setData('address_classification', $extensionAttributes->getAddressClassification());
+}
+}
+}
