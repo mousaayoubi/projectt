fa0de4f034ba0515c7ac9878230fa2a59407251d
Mousa Ayoubi <mousaayoubi@gmail.com>
2023-01-26 16:20:18 +0100

    Update files

--- app/code/Macademy/CustomCheckout/view/frontend/requirejs-config.js
+++ app/code/Macademy/CustomCheckout/view/frontend/requirejs-config.js
@@ -7,7 +7,11 @@ mixins: {
 'Magento_Checkout/js/action/set-shipping-information':
 {
 'Macademy_CustomCheckout/js/action/set-shipping-information-mixin': true
-}
-}
 },
+'Magento_Checkout/js/view/billing-address':
+{
+'Macademy_CustomCheckout/js/view/billing-address-mixin': true
+}
+}
+}
 }
